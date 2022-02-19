d68aa36e96cba1b097d7f176675ade7b0d04143e
Cengiz Ergün <cengiz1ergun@gmail.com>
2022-02-19 11:03:42 +0300

    Opr - FirstGrid - All User Requests

--- CSG/Controllers/OperatorController.cs
+++ CSG/Controllers/OperatorController.cs
@@ -1,5 +1,6 @@
 ﻿using CSG.Data;
 using CSG.Extensions;
+using CSG.Models;
 using CSG.Models.Entities.Enums;
 using CSG.Models.Identity;
 using CSG.Repository;
@@ -36,12 +37,17 @@ namespace CSG.Controllers
         {
             return View();
         }
+        #region GetAllRequests
         public IActionResult GetRequests()
         {
-            var data = _requestRepo.Get(x => x.RequestStatus == RequestStatus.Delivered).ToList();
+            //var data = _requestRepo.Get(x => x.RequestStatus == RequestStatus.Delivered).ToList();
+            var data = _requestRepo.Get().ToList();
             var query = from aur in _gizemContext.ApplicationUserRequests
                         join u in _gizemContext.Users on aur.ApplicationUserId equals u.Id
                         join r in _gizemContext.Requests on aur.RequestId equals r.Id
+                        join ur in _gizemContext.UserRoles on u.Id equals ur.UserId
+                        join rol in _gizemContext.Roles on ur.RoleId equals rol.Id
+                        where rol.Name == RoleNames.Customer || rol.Name == RoleNames.Admin || rol.Name == RoleNames.Passive
                         select new OperatorRequestViewModel
                         {
                             requestid = r.Id,
@@ -58,39 +64,42 @@ namespace CSG.Controllers
 
             return Json(new { result = DataSource, count = count });
         }
+        #endregion
 
-        public IActionResult GetTechnician()
-        {
-            var query = _requestRepo.Get();
-            var query2 = (from aur in _gizemContext.UserRoles
-                          join u in _gizemContext.Users on aur.UserId equals u.Id
-                          join r in _gizemContext.Roles on aur.RoleId equals r.Id
-                          select new
-                          {
-                              UserId = u.Id,
-                              UserName = u.UserName,
-                              RoleId = r.Id,
-                              UserRole = r.Name,
-                          });
-            var query3 = (from aur in _gizemContext.ApplicationUserRequests
-                          join u in query2 on aur.ApplicationUserId equals u.UserId
-                          join r in _gizemContext.Requests on aur.RequestId equals r.Id
-                          select new
-                          {
-                              userId = u.UserId,
-                              userName = u.UserName,
-                              roleId = r.Id,
-                              userRole = u.UserRole,
-                              requestId = aur.RequestId,
-                              request1Name = aur.Request.RequestType1.ToString(),
-                              request2Name = aur.Request.RequestType2.ToString(),
-                              requeststatus=aur.Request.RequestStatus.ToString(),
-                          });
-            var DataSource = query3.ToList();
-            int count = DataSource.Count();
 
-            return Json(new { result = DataSource, count = count });
 
-        }
+        //public IActionResult GetTechnician()
+        //{
+        //    var query = _requestRepo.Get();
+        //    var query2 = (from aur in _gizemContext.UserRoles
+        //                  join u in _gizemContext.Users on aur.UserId equals u.Id
+        //                  join r in _gizemContext.Roles on aur.RoleId equals r.Id
+        //                  select new
+        //                  {
+        //                      UserId = u.Id,
+        //                      UserName = u.UserName,
+        //                      RoleId = r.Id,
+        //                      UserRole = r.Name,
+        //                  });
+        //    var query3 = (from aur in _gizemContext.ApplicationUserRequests
+        //                  join u in query2 on aur.ApplicationUserId equals u.UserId
+        //                  join r in _gizemContext.Requests on aur.RequestId equals r.Id
+        //                  select new
+        //                  {
+        //                      userId = u.UserId,
+        //                      userName = u.UserName,
+        //                      roleId = r.Id,
+        //                      userRole = u.UserRole,
+        //                      requestId = aur.RequestId,
+        //                      request1Name = aur.Request.RequestType1.ToString(),
+        //                      request2Name = aur.Request.RequestType2.ToString(),
+        //                      requeststatus=aur.Request.RequestStatus.ToString(),
+        //                  });
+        //    var DataSource = query3.ToList();
+        //    int count = DataSource.Count();
+
+        //    return Json(new { result = DataSource, count = count });
+
+        //}
     }
 }
